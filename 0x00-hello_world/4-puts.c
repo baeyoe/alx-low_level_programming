47147e51dd71973e3a3e0775608012a1a6bba75f
baeyoe <baeyoe@yahoo.com>
2022-06-26 12:07:32 -0700

    low level programming task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stduo.h>
+/**
+ *main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"programming is like building a multlingual puzzle");
+return (0);
+}
